6575ee2b0973e15f12696de1eddfdffea83756a4
KWilkerson <kevin.j.wilkerson@gmail.com>
2022-08-13 13:08:36 -0500

    added useState and useEffect to Gallery component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./components/Nav";
 import Gallery from "./components/Gallery";
 import About from "./components/About";
 
 function App() {
+  const [categories] = useState([
+    {
+      name: "Commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "Portraits", description: "Portraits of people in my life" },
+    { name: "Food", description: "Delicious delicacies" },
+    {
+      name: "Landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
       <main>
         <Gallery></Gallery>
         <About></About>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,27 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Nav() {
-  const [categories] = useState([
-    {
-      name: "Commercial",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    { name: "Portraits", description: "Portraits of people in my life" },
-    { name: "Food", description: "Delicious delicacies" },
-    {
-      name: "Landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-  ]);
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+function Nav(props) {
+  const { categories = [], setCurrentCategory, currentCategory } = props;
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
   return (
     <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
           <span role="img" aria-label="camera">
+            {" "}
             📸
           </span>
           Oh Snap!
